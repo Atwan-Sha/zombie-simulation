2d87acd7dab40e3e52b31865a9c78d6120553de6
m.pruglo <michael.pruglo.work@gmail.com>
2023-03-28 19:27:02 +0300

    refactor

--- model.js
+++ model.js
@@ -1,6 +1,64 @@
-const GRID_SIZE = 10;
-const REPRODUCTIVE_AGE = 2;
-const INITIAL_POPULATION = 5;
+class GameModel {
+    constructor(cfg) {
+        for (let prop of ["grid_size", "reproductive_age", "initial_population"]) {
+            console.assert(cfg.hasOwnProperty(prop));
+        }
+        this.impl = new GameModelImpl(cfg);
+    }
+    get_grid(timestep) {
+        return this.impl.get_grid(timestep);
+    }
+}
+
+
+
+
+
+
+class GameModelImpl {
+    constructor(cfg) {
+        this.curr_timestep = 0;
+        this.cfg = cfg;
+        this.population = Array.from(Array(cfg.initial_population), () => Creature.create_rand(cfg.grid_size));
+        this.refresh_grid();
+    }
+
+    get_grid(timestep) {
+        if (timestep < this.curr_timestep) {
+            throw new Error("cannot go backwards in time yet, create new GameModel");
+        }
+        while (this.curr_timestep < timestep) {
+            this.turn();
+            ++this.curr_timestep;
+        }
+        return this.grid;
+    }
+
+    turn() {
+        for (let c of this.population)
+            c.age++;
+
+        for (let dad of this.reproductives("Male")) {
+            for (let mom of this.reproductives("Female")) {
+                let child = Creature.from_mom(mom);
+                this.population.push(child);
+            }
+        }
+
+        this.refresh_grid();
+    }
+    reproductives(sex) {
+        return this.population.filter(c => c.sex==sex && c.age>=this.cfg.reproductive_age);
+    }
+
+    refresh_grid() {
+        this.grid = Array.from(Array(this.cfg.grid_size), () => Array(this.cfg.grid_size));
+        for (let c of this.population) {
+            this.grid[c.y][c.x] = c;
+        }
+    }
+}
+
 class Creature {
     sex;
     color;
@@ -9,17 +67,31 @@ class Creature {
     is_radioactive;
     x; y;
 
-    letter() {
-        if (this.is_radioactive)
-            return "X";
-        var lett = this.sex[0];
-        if (this.age < REPRODUCTIVE_AGE)
-            lett = lett.toLowerCase()
-        return lett
+    constructor() {
+        this.sex = rand_choice(sexes);
+        this.age = 0;
+        this.name = rand_choice(possible_names);
+        this.is_radioactive = false;
+    }
+
+    static from_mom(mom) {
+        let c = new Creature();
+        c.color = mom.color;
+        c.x = mom.x + 1;
+        c.y = mom.y + 1;
+        return c;
+    }
+
+    static create_rand(grid_size) {
+        let c = new Creature();
+        c.color = rand_choice(colors);
+        c.x = randint(grid_size - 1);
+        c.y = randint(grid_size - 1);
+        return c;
     }
 
     toString() {
-        return `${this.age} ${this.sex} ${this.color} ${this.name} ${this.is_radioactive} ${this.x} ${this.y} ${this.letter()}`
+        return `${this.age}`// ${this.sex} ${this.color} ${this.name} ${this.is_radioactive} ${this.x} ${this.y}`
     }
 }
 
@@ -29,59 +101,23 @@ function randint(max) {
 function rand_choice(array) {
     return array[randint(array.length)];
 }
-function place_map(x, y, letter) {
-    crmap[y] = crmap[y].substring(0, x) + letter + crmap[y].substring(x+1);
-}
 
 const sexes = ["Male", "Female"];
 const colors = ["White", "Red", "Green"]
 const possible_names = ["Alice", "Bob"];
-var crmap = Array.from(Array(GRID_SIZE), () => ".".repeat(GRID_SIZE));
-var population = Array.from(Array(INITIAL_POPULATION), () => rand_creature());
-
-function rand_creature(mom) {
-    var c = new Creature();
-    c.sex = rand_choice(sexes);
-    c.age = 0;
-    c.name = rand_choice(possible_names);
-    c.is_radioactive = false;
-    if (mom === undefined) {
-        c.color = rand_choice(colors);
-        c.x = randint(GRID_SIZE-1);
-        c.y = randint(GRID_SIZE-1);
-    } else {
-        c.color = mom.color;
-        c.x = mom.x + 1;
-        c.y = mom.y + 1;
-    }
-    place_map(c.x, c.y, c.letter());
-    return c;
-}
-
-function turn() {
-    for (c of population) c.age++;
-
-    function reproductives(sex) { return population.filter(creature => creature.sex==sex && creature.age>=REPRODUCTIVE_AGE)}
-    for (dad of reproductives("Male")) {
-        for (mom of reproductives("Female")) {
-            child = rand_creature(mom);
-            population.push(child);
-        }
-    }
-
-    crmap = Array.from(Array(GRID_SIZE), () => ".".repeat(GRID_SIZE));
-    for (c of population) place_map(c.x, c.y, c.letter());
 
 
-}
 
-function display() {
-    console.log("population: ", population.length);
-    for (c of population) console.log(String(c));
-    console.log(crmap.join("\n"));
-}
 
-for (var t = 0; t < 5; t++) {
-    display();
-    turn();
+
+
+
+let model = new GameModel({
+    initial_population: 5,
+    reproductive_age: 2,
+    grid_size: 10,
+})
+let grid = model.get_grid(2);
+for (let row of grid) {
+    console.log(String(row));
 }
