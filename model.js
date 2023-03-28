1e7c50e4e357d657b3e5c63ce8bc6382d1887d24
m.pruglo <michael.pruglo.work@gmail.com>
2023-03-28 12:30:47 +0300

    map update

--- model.js
+++ model.js
@@ -1,21 +1,25 @@
-const GRID_SIZE = 80;
+const GRID_SIZE = 10;
 const REPRODUCTIVE_AGE = 2;
 const INITIAL_POPULATION = 5;
 class Creature {
-    sex = "Male";
-    color = "White";
-    age = 0;
-    name = "Bob";
-    is_radioactive = false;
+    sex;
+    color;
+    age;
+    name;
+    is_radioactive;
     x; y;
 
     letter() {
         if (this.is_radioactive)
             return "X";
-        lett = this.sex[0];
+        var lett = this.sex[0];
         if (this.age < REPRODUCTIVE_AGE)
             lett = lett.toLowerCase()
         return lett
+    }
+
+    toString() {
+        return `${this.age} ${this.sex} ${this.color} ${this.name} ${this.is_radioactive} ${this.x} ${this.y} ${this.letter()}`
     }
 }
 
@@ -25,15 +29,18 @@ function randint(max) {
 function rand_choice(array) {
     return array[randint(array.length)];
 }
+function place_map(x, y, letter) {
+    crmap[y] = crmap[y].substring(0, x) + letter + crmap[y].substring(x+1);
+}
 
 const sexes = ["Male", "Female"];
 const colors = ["White", "Red", "Green"]
 const possible_names = ["Alice", "Bob"];
-var population = [];
-var map = Array.from(Array(GRID_SIZE), () => " ".repeat(GRID_SIZE));
+var crmap = Array.from(Array(GRID_SIZE), () => ".".repeat(GRID_SIZE));
+var population = Array.from(Array(INITIAL_POPULATION), () => rand_creature());
 
 function rand_creature(mom) {
-    c = new Creature();
+    var c = new Creature();
     c.sex = rand_choice(sexes);
     c.age = 0;
     c.name = rand_choice(possible_names);
@@ -47,24 +54,34 @@ function rand_creature(mom) {
         c.x = mom.x + 1;
         c.y = mom.y + 1;
     }
+    place_map(c.x, c.y, c.letter());
     return c;
 }
 
 function turn() {
+    for (c of population) c.age++;
+
     function reproductives(sex) { return population.filter(creature => creature.sex==sex && creature.age>=REPRODUCTIVE_AGE)}
-    for (dad in reproductives("Male")) {
-        for (mom in reproductives("Female")) {
+    for (dad of reproductives("Male")) {
+        for (mom of reproductives("Female")) {
             child = rand_creature(mom);
             population.push(child);
-            map[child.x][child.y] = child.letter();
         }
     }
+
+    crmap = Array.from(Array(GRID_SIZE), () => ".".repeat(GRID_SIZE));
+    for (c of population) place_map(c.x, c.y, c.letter());
 
 
 }
 
-population = Array.from(Array(INITIAL_POPULATION), () => rand_creature());
-for (var t = 0; t < 2; t++) {
-    console.log(population, map.join("\n"));
+function display() {
+    console.log("population: ", population.length);
+    for (c of population) console.log(String(c));
+    console.log(crmap.join("\n"));
+}
+
+for (var t = 0; t < 5; t++) {
+    display();
     turn();
 }
