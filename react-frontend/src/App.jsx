572bf6e41791056b4ea3ce7185868740bde699cb
Atwan-Sha <atwanshaaban@gmail.com>
2023-09-29 21:29:32 +0200

    setup React with Vite bundle

---
+++ react-frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Button from './Components.jsx'
+
+function App() {
+  return (
+    <>
+      <h1>Hello Zombies!</h1>
+      <Button />
+    </>
+  )
+}
+
+export default App
