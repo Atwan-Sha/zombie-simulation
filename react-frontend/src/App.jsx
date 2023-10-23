915f1f83ba00739373ee66e332c519a6bb2c8447
Atwan-Sha <atwanshaaban@gmail.com>
2023-10-23 22:16:08 +0200

    create and test grid component prototype

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -1,13 +1,14 @@
-import './App.css'
-import Button from './Components.jsx'
+import { Button, Grid } from "./Components.jsx";
+import TEST_GRID from "./testData.js";
 
 function App() {
   return (
     <>
       <h1>Hello Zombies!</h1>
       <Button />
+      <Grid gridData={TEST_GRID} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- react-frontend/src/Components.jsx
+++ react-frontend/src/Components.jsx
@@ -1,7 +1,8 @@
-import { useState } from "react"
+import { useState } from "react";
+// import { TEST_GRID } from "./testData.js"
 
-export default function Button() {
-  const [count, setCount] = useState(0)
+export function Button() {
+  const [count, setCount] = useState(0);
   const handleClick = () => {
     setCount(count + 1);
   };
@@ -9,6 +10,39 @@ export default function Button() {
   return (
     <>
       <button onClick={handleClick}>{count}</button>
+    </>
+  );
+}
+
+export function Grid({ gridData }) {
+  console.log("render grid...");
+
+  const gridCells = [];
+  let key = 0;
+  gridData.forEach((row) => {
+    row.forEach((obj) => {
+      gridCells.push(
+        <div
+          key={key}
+          className="cell"
+          style={{
+            backgroundColor:
+              obj === null || Object.keys(obj).length === 0
+                ? "none"
+                : obj.color.toLowerCase(),
+          }}
+        >
+          {obj === null || Object.keys(obj).length === 0 ? "" : obj.color}
+        </div>
+      );
+      key++;
+    });
+  });
+
+  return (
+    // generate size of grid from input cfg
+    <>
+      <div className="grid-container">{gridCells}</div>
     </>
   );
 }
